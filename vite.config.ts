a5f3efdcffaaba89bea05580658341927a608eed
Azharjeee <142223471+Azharjeee@users.noreply.github.com>
2026-08-21 21:32:17 -0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-base: '/moster-jumper/',
+base: './',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
